52edfd37c5b4637773b12e03d6f7991943d38466
YOKESH <yokesh.codewents@gmail.com>
2023-03-13 11:42:13 +0530

    New project

---
+++ app/src/main/java/com/example/sam1project/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.sam1project;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonSendEmail(View view){
+
+        try {
+            String stringSenderEmail = "yokeshbaskaran@gmail.com";
+            String stringPasswordSenderEmail = "robjidzzuiaicnql";
+
+            String stringReceiverEmail = "yokesh.codewents@gmail.com";
+
+            String stringHost = "smtp.gmail.com";
+
+            Properties properties = System.getProperties();
+
+            properties.put("mail.smtp.host", stringHost);
+            properties.put("mail.smtp.port", "465");
+            properties.put("mail.smtp.ssl.enable", "true");
+            properties.put("mail.smtp.auth", "true");
+
+            javax.mail.Session session = Session.getInstance(properties, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(stringSenderEmail,
+                            stringPasswordSenderEmail);
+                }
+            });
+
+            MimeMessage mimeMessage = new MimeMessage(session);
+            mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(stringReceiverEmail));
+
+            mimeMessage.setSubject("Verification email:");
+            mimeMessage.setText("Hello User," +
+                    " \nThis is a verification email." +
+                    "\n Username:admin" +
+                    "\n Password:admin" +
+                    "\n***********");
+
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Transport.send(mimeMessage);
+                    } catch (MessagingException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            thread.start();
+
+        } catch (AddressException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+}
